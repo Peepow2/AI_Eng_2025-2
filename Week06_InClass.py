5ba24edda1580f2e4aa3011612fb7dc31d99c7d8
Peepow2 <85884765+Peepow2@users.noreply.github.com>
2026-02-12 21:58:56 +0700

    Update Week06_InClass.py

--- Week06_InClass.py
+++ Week06_InClass.py
@@ -1,5 +1,10 @@
+# Warning !!!
+# At Variale selection (manually) Cell 
+# Change x to x = x[['RM', 'PTRATIO', 'LSTAT']]
+
 MSE = sklearn.metrics.mean_squared_error(y_test, y_pred)
 
+print(f"MEDV = {model.intercept_} + {model.coef_[0]}*RM + {model.coef_[1]}*PTRATIO + {model.coef_[2]}*LSTAT")
 print(f"RMSE = {math.sqrt(MSE):.2f}")
 print(f"Average house price = {y_test.mean():.2f}")
 print(f"R_square = {model.score(x_test,y_test):.2f}")
